2008dfd7404bb51de7a464d37d511cd04b292521
Alex Alarud <43539305+AlexAlarud@users.noreply.github.com>
2021-05-24 14:37:09 +0200

    Fils for setting up DB and preset values

---
+++ DB-Setup_insert_pre-set_Categorys.sql
@@ -0,0 +1,7 @@
+INSERT INTO Category Values ('Flower Charger Pattern')
+INSERT INTO Category VALUES ('Creeper Charger Pattern')
+INSERT INTO Category Values ('Skull Charger Pattern')
+INSERT INTO Category Values ('Thing Pattern')
+INSERT INTO Category Values ('Snout Pattern')
+INSERT INTO Category Values ('Globe Pattern')
+INSERT INTO Category Values ('No Pattern used')
